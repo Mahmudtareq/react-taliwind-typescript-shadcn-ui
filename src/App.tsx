3c2a4e78fc959cb49b2d8d001b8be0b4bc149e4c
Mahmudtareq <tareqlpu5@gmail.com>
2024-01-24 11:02:02 +0600

    react + taliwind + typescript + shadcn/ui project setup template

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "./components/ui/button";
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Button>Click Me</Button>
+    </div>
+  );
+};
+export default App;
